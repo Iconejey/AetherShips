cc28a151a3ed3615c6a7f6a00905a14e7a1984e1
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2026-03-19 14:12:59 +0100

    Added edit mode to terminal with hovered block type

--- js/components/user-terminal.js
+++ js/components/user-terminal.js
@@ -1,28 +1,70 @@
 class UserTerminal extends HTMLElement {
 	async connectedCallback() {
-		this.innerHTML = html`
-			<div class="line">U.R.A. OS version <span id="version"></span></div>
-			<div class="line">Sector [<span id="sector"></span>] position (<span id="coords"></span>)</div>
-		`;
-
 		const pkg = await fetch('package.json').then(r => r.json());
-		this.$('#version').textContent = pkg.version;
-
-		setInterval(() => {
-			const followed = window.game?.camera?.followed_entity;
-			if (!followed) return;
-
-			const global_x = followed.position.x / 32;
-			const global_y = followed.position.y / 32;
-
-			const chunk_x = Math.floor(global_x % 256);
-			const chunk_y = Math.floor(global_y % 256);
-			this.$('#coords').textContent = `${chunk_x}, ${chunk_y}`;
-
-			const sector_x = Math.floor(global_x / 256);
-			const sector_y = Math.floor(global_y / 256);
-			this.$('#sector').textContent = `${sector_x}, ${sector_y}`;
+		this.version = pkg.version;
+		this.tick_interval_id = window.setInterval(() => {
+			if (game?.mode !== this.current_mode) this.mode = game?.mode;
+			this.tick?.();
 		}, 250);
+	}
+
+	set mode(mode) {
+		this.current_mode = mode;
+		this.classList.toggle('opaque', mode === 'start_menu');
+
+		if (mode === 'navigation') {
+			this.innerHTML = html`
+				<div class="line">U.R.A. OS version ${this.version} - Day 1</div>
+				<div class="line">Sector [<span id="sector"></span>] position (<span id="coords"></span>)</div>
+			`;
+
+			this.tick = () => {
+				const followed = window.game?.camera?.followed_entity;
+				if (!followed) return;
+
+				const global_x = followed.position.x / 32;
+				const global_y = followed.position.y / 32;
+
+				const chunk_x = Math.floor(global_x % 256);
+				const chunk_y = Math.floor(global_y % 256);
+				this.$('#coords').textContent = `${chunk_x}, ${chunk_y}`;
+
+				const sector_x = Math.floor(global_x / 256);
+				const sector_y = Math.floor(global_y / 256);
+				this.$('#sector').textContent = `${sector_x}, ${sector_y}`;
+			};
+
+			return;
+		}
+
+		if (mode === 'edit') {
+			this.innerHTML = html`
+				<div class="line">U.R.A. OS version ${this.version} - Day 1</div>
+				<div class="line">Type : <span id="type"></span></div>
+			`;
+
+			this.tick = () => {
+				if (game?.mode !== this.current_mode) return (this.mode = game?.mode ?? 'navigation');
+
+				const followed_entity = game?.camera?.followed_entity;
+				const edit_preview = $('edit-preview');
+				if (!followed_entity || !edit_preview) {
+					this.$('#type').textContent = '-';
+					return;
+				}
+
+				const hovered_block = edit_preview.screenToBlock(edit_preview.mouse_x, edit_preview.mouse_y);
+				if (!hovered_block) {
+					this.$('#type').textContent = '-';
+					return;
+				}
+
+				const block_info = followed_entity.getBlockInfo(game.selected_layer, hovered_block.bx, hovered_block.by);
+				this.$('#type').textContent = block_info.is_empty ? 'empty' : (blocks_by_type[block_info.type]?.name ?? `${block_info.type}`);
+			};
+
+			return;
+		}
 	}
 }
 
