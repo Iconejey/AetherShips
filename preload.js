2efcdfef7ca0c0b20e32377b6884ac8b37c5b2c5
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2026-03-27 16:20:56 +0100

    Renamed ipcRendere to ipc

--- preload.js
+++ preload.js
@@ -1,31 +1,31 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer: ipc } = require('electron');
 const path = require('path');
 const figlet = require('figlet');
 
 figlet.defaults({ fontPath: path.join(__dirname, 'node_modules', 'figlet', 'fonts') });
 
 contextBridge.exposeInMainWorld('saves', {
-	listGalaxies: () => ipcRenderer.invoke('save-list-galaxies'),
-	createGalaxy: galaxy_name => ipcRenderer.invoke('save-create-galaxy', galaxy_name),
-	deleteGalaxy: galaxy_name => ipcRenderer.invoke('save-delete-galaxy', galaxy_name),
-	writeGalaxy: galaxy => ipcRenderer.invoke('save-write-galaxy', galaxy),
-	loadGalaxy: galaxy_name => ipcRenderer.invoke('save-load-galaxy', galaxy_name),
-	writeEntity: (galaxy_name, serialized_entity) => ipcRenderer.invoke('save-write-entity', galaxy_name, serialized_entity),
-	loadEntities: (galaxy_name, position) => ipcRenderer.invoke('save-load-entities', galaxy_name, position),
+	listGalaxies: () => ipc.invoke('save-list-galaxies'),
+	createGalaxy: galaxy_name => ipc.invoke('save-create-galaxy', galaxy_name),
+	deleteGalaxy: galaxy_name => ipc.invoke('save-delete-galaxy', galaxy_name),
+	writeGalaxy: galaxy => ipc.invoke('save-write-galaxy', galaxy),
+	loadGalaxy: galaxy_name => ipc.invoke('save-load-galaxy', galaxy_name),
+	writeEntity: (galaxy_name, serialized_entity) => ipc.invoke('save-write-entity', galaxy_name, serialized_entity),
+	loadEntities: (galaxy_name, position) => ipc.invoke('save-load-entities', galaxy_name, position),
 
 	writeLayerChunk: (galaxy_name, serialized_entity, layer_index, chunk_x, chunk_y, type, uint32_array) => {
 		const buffer = Buffer.from(uint32_array.buffer, uint32_array.byteOffset, uint32_array.byteLength);
-		return ipcRenderer.invoke('save-write-layer-chunk', galaxy_name, serialized_entity, layer_index, chunk_x, chunk_y, type, buffer);
+		return ipc.invoke('save-write-layer-chunk', galaxy_name, serialized_entity, layer_index, chunk_x, chunk_y, type, buffer);
 	},
 
 	loadLayerChunk: async (galaxy_name, serialized_entity, layer_index, chunk_x, chunk_y, type) => {
-		const data = await ipcRenderer.invoke('save-load-layer-chunk', galaxy_name, serialized_entity, layer_index, chunk_x, chunk_y, type);
+		const data = await ipc.invoke('save-load-layer-chunk', galaxy_name, serialized_entity, layer_index, chunk_x, chunk_y, type);
 		return new Uint32Array(data.buffer, data.byteOffset, data.byteLength / 4);
 	},
 
-	listChunks: (galaxy_name, serialized_entity, layer_index) => ipcRenderer.invoke('save-list-chunks', galaxy_name, serialized_entity, layer_index),
-	clean: galaxy_name => ipcRenderer.invoke('save-clean', galaxy_name),
-	finalize: galaxy_name => ipcRenderer.invoke('save-finalize', galaxy_name)
+	listChunks: (galaxy_name, serialized_entity, layer_index) => ipc.invoke('save-list-chunks', galaxy_name, serialized_entity, layer_index),
+	clean: galaxy_name => ipc.invoke('save-clean', galaxy_name),
+	finalize: galaxy_name => ipc.invoke('save-finalize', galaxy_name)
 });
 
 contextBridge.exposeInMainWorld('figlet', (text, options) => {
